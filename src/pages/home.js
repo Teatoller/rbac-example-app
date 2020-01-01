5fad2c628cb73ba316d9ca71e688f4cef6643bb6
Teatoller <steven.otieno1@gmail.com>
2020-01-01 12:59:59 +0300

    (auth): Implement auth
    - add auth variables
    - add context api that exposes six props
    - install auth
    - add auth component
    - import Auth and wrap App router components with it
    - add a callback component in pages
    - rename callback component
    - add route for callbackPage component in app
    - add a login button component
    - add a logout button component
    - update home component
    - create Postlist component
    - add postlist to home component
    
    [Starts #003]

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,27 @@
 import React from "react";
+import { Redirect, NavLink } from "react-router-dom";
+import { AuthConsumer } from "../authContext";
+import Login from "../components/Login";
+// import PostsList from "../components/PostsList";
 
-const HomePage = () => <h2>Home</h2>;
+const HomePage = () => (
+  <AuthConsumer>
+    {({ authenticated }) =>
+      authenticated ? (
+        <Redirect to="/dashboard" />
+      ) : (
+        <div>
+          <h2>
+            <NavLink to="https://auth0.com/blog/role-based-access-control-rbac-and-react-apps/#Introduction-to-Role-Based-Access-Control">
+              Welcome to React RBAC Tutorial.
+            </NavLink>
+          </h2>
+          <Login />
+          {/* <PostsList /> */}
+        </div>
+      )
+    }
+  </AuthConsumer>
+);
 
 export default HomePage;
